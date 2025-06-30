92e5cf9f06a31bcebfa7b077394568c4441715db
Micho-Tade <tadessetabuse@gmail.com>
2025-06-30 22:58:30 +0300

    idon't know why this happend but here is another commit i guess

--- src/Pages/ProductDetail/ProductDetail.jsx
+++ src/Pages/ProductDetail/ProductDetail.jsx
@@ -2,38 +2,45 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { productUrl } from '../../api/endpoints';
-import Classes from './ProductDetail.module.css'
+import Classes from './ProductDetail.module.css';
 import LayOut from '../../Components/LayOut/LayOut';
 import ProductCard from '../../Components/Product/ProductCard';
 import Loader from '../../Components/Loader/Loader';
 
 function ProductDetail() {
   const { productId } = useParams();
-  const [product, setProduct] = useState([]);
-  const [isLoading, setIsLoading] = useState(false)
+  const [product, setProduct] = useState(null); // Use null for single product
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
-    setIsLoading(true)
-    axios.get(`${productUrl}/products/${productId}`)
+    setIsLoading(true);
+    axios
+      .get(`${productUrl}/products/${productId}`)
       .then((res) => {
         setProduct(res.data);
-        setIsLoading(false)
+        setIsLoading(false);
       })
       .catch((err) => {
         console.log(err);
-        setIsLoading(false)
+        setIsLoading(false);
       });
-  }, []);
+  }, [productId]); // Add productId as dependency
 
   return (
     <LayOut>
-        {isLoading? (<Loader />):(
+      {isLoading ? (
+        <Loader />
+      ) : product ? (
         <ProductCard
-        product={product}
-        flex={true}
-        renderDesc={true}
-        renderAdd={true} />)}
+          product={product}
+          flex={true}
+          renderDesc={true}
+          renderAdd={true}
+        />
+      ) : (
+        <div className={Classes.error}>Product not found.</div>
+      )}
     </LayOut>
-  )
+  );
 }
 
 export default ProductDetail;
