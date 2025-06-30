5cd95c7851c42962d7990c36369e12231a3eea5e
Micho-Tade <tadessetabuse@gmail.com>
2025-06-30 22:38:25 +0300

    commiting

--- src/Api/axios.jsx
+++ src/Api/axios.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-2025-96bdb/us-central1/api" // Use only the emulator's base URL
+  /* baseURL: "http://127.0.0.1:5001/clone-2025-96bdb/us-central1/api" // Use only the emulator's base URL */
+  baseURL: "https://amazon-backend-eicz.onrender.com" // Use the live version on render
 });
 
 export default axiosInstance;
